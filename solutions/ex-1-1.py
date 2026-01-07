2707ef6c7c5d504086e3d7b3b0d9f481c6ecbf6e
LuSkywalker <callumlu@gmail.com>
2026-01-07 15:54:37 +0900

    [A] finish 1-b

--- solutions/ex-1-1.py
+++ solutions/ex-1-1.py
@@ -23,5 +23,14 @@ print(f"[ii] {final}")
 print("\nb)")
 
 input_num = input("please enter a number...")
-print(f"[i] cube: {pow(int(input_num), 3)}, square: {math.pow(int(input_num), 2)}")
+print(f"[i] cube: {pow(int(input_num), 3)}, square: {math.pow(int(input_num), 2)}\n")
 
+str1 = input("please enter first string...")
+str2 = input("please enter second string...")
+print(f"[ii] {str1 + str2}\n")
+
+answers = [1, 17, 42]
+check_num = input("please enter a number to check...")
+
+check_res = int(check_num) in answers
+print(f"[i] {check_res}\n")
